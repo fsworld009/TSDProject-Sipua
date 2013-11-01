6f78fe2c53da93c97e01794818c1f506165abcd2
WorldFS <fsworld009@gmail.com>
2013-11-01 15:31:43 -0500

    second commit

--- src/sipua/SipUA.java
+++ src/sipua/SipUA.java
@@ -4,10 +4,26 @@
  */
 package sipua;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.zoolu.sip.provider.SipProvider;
+
 /**
  *
  * @author WorldFS
  */
 public class SipUA {
+    SipProvider sipProvider;
     
+    public SipUA(int port){
+        try {
+            SipProvider sipProvider = new SipProvider(InetAddress.getLocalHost().getHostAddress(),port);
+        } catch (UnknownHostException ex) {
+            //Logger.getLogger(SipUA.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("no such host");
+        }
+        
+    }
 }
